1bf004367c1ccc39f2c590cf0389be12420fc80d
Ganapathy77 <106886401+Ganapathy77@users.noreply.github.com>
2023-03-19 18:22:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ function threeSum(arr, target) {
   let closetSum = Number.MAX_VALUE;
 
 	for(let i=0; i<arr.length; i++){
-		for(let j=0; j<arr.length; j++){
-			for(let k=0; k<arr.length; k++){
+		for(let j=i+1; j<arr.length; j++){
+			for(let k=j+1; k<arr.length; k++){
 				if(Math.abs(target - closetSum)> Math.abs(target - (arr[i]+arr[j]+arr[k]))){
 					closetSum = Math.abs(arr[i]+arr[j]+arr[k])
 				}
